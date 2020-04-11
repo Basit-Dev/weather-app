02a4f747c5069a64916ced6e16ba8d1dfb11bdd2
bazz1551 <aha1551@gmail.com>
2020-04-11 01:40:14 +0100

    Added gps package and location coordinate grabber. Setup API and grabbed json data.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,18 +12,39 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    // await means do not print the lon/lat until location.getCurrentLocation has completed.
+    print(location.longitude);
+    print(location.latitude);
+  }
+
+  void getData() async {
+    http.Response response = await http
+        .get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double temp = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = jsonDecode(data)['name'];
+      print('$temp $condition $cityName');
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: RaisedButton(
-            onPressed: () async {
-              //Get the current location
-            },
-            child: Text('Get Location'),
-          ),
-        ),
-      ),
-    );
+    getData();
+    return Scaffold();
   }
 }
---
+++ scratch.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'dart:io';
+
+void main() {
+  performTasks();
+}
+
+void performTasks() async {
+  task1();
+  String result;
+
+  /// data gets returned to this variable as its the initialisation variable
+
+  String test = await task2(result);
+  task3(result);
+
+  /// you cant pass the result parameter in there you have to create another variable like  the test variable
+}
+
+void task1() {
+  String result = 'task 1 data';
+  print('Task 1 complete');
+}
+
+Future<String> task2(String result) async {
+  Duration threeSeconds = Duration(seconds: 3);
+  //String result;
+  await Future.delayed(threeSeconds, () {
+    result = 'ji';
+    print('Task 2 complete $result');
+  });
+
+  return result;
+}
+
+void task3(result) {
+  //String result = 'task 3 data';
+  print('Task 3 complete $result');
+}
